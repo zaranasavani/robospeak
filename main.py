85438aefd15d614fa7a4211f3b800d32ceb4d24d
2109zarana <zarana.2002.savani@gmail.com>
2024-01-31 21:47:22 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+
+if __name__ == '__main__':
+    s = "Welcome to RoboSpeaker "
+    command1 = f"PowerShell -Command "f"Add-Type -AssemblyName System.Speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{s}');"
+    os.system(command1)
+    while True:
+        x = input("Enter What Do You Want To Say")
+        if x=='q':
+            b = "bye bye friends"
+            command2 = f"PowerShell -Command "f"Add-Type -AssemblyName System.Speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{b}');"
+            os.system(command2)
+            break
+
+        command = f"PowerShell -Command "f"Add-Type -AssemblyName System.Speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{x}');"
+        os.system(command)
+
+
+
+
