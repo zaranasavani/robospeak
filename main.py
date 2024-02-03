8bdf4ff6b5bfc480e256405037a804c41e4f26e3
zarana savani <129183177+zaranasavani@users.noreply.github.com>
2024-02-03 23:12:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import win32com.client as wincom
 
 if __name__ == '__main__':
-    s = "Welcome to RoboSpeaker "
+    speak = wincom.Dispatch(SAPI.SpVoice)
+    hello = "Welcome to RoboSpeaker "
+    speak.Speak(hello)
     while True:
-        speak = wincom.Dispatch(SAPI.SpVoice)
         x = input("Enter What Do You Want To Say)
         if x=='q':
             b = "bye bye friends"
