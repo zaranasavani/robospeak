6f9e16ea6a343e87dafb13120d6ab20195cc1afe
2109zarana <zarana.2002.savani@gmail.com>
2024-03-05 23:22:08 +0530

    first commit

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
-import os
+# import os
+#
+# if __name__ == '__main__':
+#     s = "Welcome to RoboSpeaker "
+#     command1 = f"PowerShell -Command "f"Add-Type -AssemblyName System.Speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{s}');"
+#     os.system(command1)
+#     while True:
+#         x = input("Enter What Do You Want To Say")
+#         if x=='q':
+#             b = "bye bye friends"
+#             command2 = f"PowerShell -Command "f"Add-Type -AssemblyName System.Speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{b}');"
+#             os.system(command2)
+#             break
+#
+#         command = f"PowerShell -Command "f"Add-Type -AssemblyName System.Speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{x}');"
+#         os.system(command)
 
-if __name__ == '__main__':
-    s = "Welcome to RoboSpeaker "
-    command1 = f"PowerShell -Command "f"Add-Type -AssemblyName System.Speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{s}');"
-    os.system(command1)
+
+import win32com.client as wincom
+
+if __name__=='__main__':
+    speak = wincom.Dispatch("SAPI.SpVoice")
+    hello = "welcome to robospeak by zarana"
+    speak.Speak(hello)
     while True:
-        x = input("Enter What Do You Want To Say")
-        if x=='q':
-            b = "bye bye friends"
-            command2 = f"PowerShell -Command "f"Add-Type -AssemblyName System.Speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{b}');"
-            os.system(command2)
+
+        x=input("Enter what you want to say:")
+        if x=="q":
+            b="bye bye frieds"
+            speak.Speak(b)
             break
+        speak.Speak(x)
 
-        command = f"PowerShell -Command "f"Add-Type -AssemblyName System.Speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{x}');"
-        os.system(command)
-
+print("the end")
 
 
 
