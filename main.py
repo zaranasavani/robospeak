2bdf778a6a762764ce939e5a80db8bb4924cb2be
zarana savani <129183177+zaranasavani@users.noreply.github.com>
2024-03-23 22:39:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
 
-# import os
-#
-# if __name__ == '__main__':
-#     s = "Welcome to RoboSpeaker "
-#     command1 = f"PowerShell -Command "f"Add-Type -AssemblyName System.Speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{s}');"
-#     os.system(command1)
-#     while True:
-#         x = input("Enter What Do You Want To Say")
-#         if x=='q':
-#             b = "bye bye friends"
-#             command2 = f"PowerShell -Command "f"Add-Type -AssemblyName System.Speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{b}');"
-#             os.system(command2)
-#             break
-#
-#         command = f"PowerShell -Command "f"Add-Type -AssemblyName System.Speech;(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{x}');"
-#         os.system(command)
-
-
 import win32com.client as wincom
 
 if __name__=='__main__':
